684e43a19539f20bf6373281897be62a5a2fefc0
Jose Sosa <jose.sosa@knights.ucf.edu>
2020-04-25 01:15:00 -0400

    Partial solution to Interview Question 1.2. More unit tests needed.

--- src/main/java/com/imbuehub/cci/chapterone/ChapterOne.java
+++ src/main/java/com/imbuehub/cci/chapterone/ChapterOne.java
@@ -4,6 +4,12 @@ import java.util.Arrays;
 
 public class ChapterOne {
 
+  /**
+   * Interview Question 1.1 - Variation allows for the use of Data Structure.
+   *
+   * @param s is a string of any size.
+   * @return True if the string is made up of unique characters. Otherwise, return false.
+   */
   public boolean isUnique(String s) {
     if (s.length() <= 1) return true;
     else if (s.length() == 2) return s.charAt(0) != s.charAt(1);
@@ -18,11 +24,68 @@ public class ChapterOne {
     }
   }
 
+  /**
+   * Interview Question 1.1 - Variation does not allows for the use of Data Structure.
+   *
+   * @param s is a string of any size.
+   * @return True if the string is made up of unique characters. Otherwise, return false.
+   */
   public boolean isUniqueNoDataStructure(String s) {
     char[] chars = s.toCharArray();
     Arrays.sort(chars);
     for (int i = 0; i < chars.length - 1; i++) {
       if (chars[i] == chars[i + 1]) return false;
+    }
+    return true;
+  }
+
+  /**
+   * Interview Question 1.2 - Variation allows fo the use of Data Structure.
+   *
+   * @param s1 is a string of any size.
+   * @param s2 is a string of any size.
+   * @return True if the strings are a permutation of each other. Otherwise, return false.
+   */
+  public boolean isPermutation(String s1, String s2) {
+    if (s1.equals(s2)) return true;
+    else if (s1.length() != s2.length()) return false;
+    else {
+      int[] charCount = addCharsSeenInFirstStringAndSubtractCharsSeenInSecondString(s1, s2);
+      return allCharCountsAreZero(charCount);
+    }
+  }
+
+  /**
+   * Interview Question 1.2 - This is a helper method used to sum how many times characters appear
+   * in the first string and subtracts how many times Sums how characters appear on the second
+   * string.
+   *
+   * @param s1 is a string of size n.
+   * @param s2 is a string of size n.
+   * @return int[] where each element position correspond to an ASCII character and each element
+   *     value is the result of the sum and subtraction of how many time each corresponding ASCII
+   *     character is present in each string.
+   */
+  private int[] addCharsSeenInFirstStringAndSubtractCharsSeenInSecondString(String s1, String s2) {
+    int[] charCount = new int[255];
+    Arrays.fill(charCount, 0);
+    for (int i = 0; i < s1.length(); i++) {
+      charCount[s1.charAt(i)]++;
+      charCount[s2.charAt(i)]--;
+    }
+    return charCount;
+  }
+
+  /**
+   * Interview Question 1.2 - This is a helper method. If any element is not zero, then it means the
+   * strings in the caller method are not permutations of each other.
+   *
+   * @param charCounts is an array where each element position represents an ASCII character.
+   * @return false if any element has contains a number other than zero.
+   */
+  private boolean allCharCountsAreZero(int[] charCounts) {
+    for (int c : charCounts) {
+      if (c != 0) return false;
     }
     return true;
   }
---
+++ src/test/java/com/imbuehub/cci/chapterone/IsPermutationTests.java
@@ -0,0 +1,31 @@
+package com.imbuehub.cci.chapterone;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class IsPermutationTests {
+  private final ChapterOne chapterOne = new ChapterOne();
+
+  @Test
+  public void testTwoEqualSingleCharStringsArePermutations() {
+    assertTrue(
+        "String 'a' is permutation of string 'a' and should return true",
+        chapterOne.isPermutation("a", "a"));
+  }
+
+  @Test
+  public void testTwoDifferentSingleCharStringAreNotPermutations() {
+    assertFalse(
+        "String 'a' is not a permutation of string 'b' and should return false",
+        chapterOne.isPermutation("a", "b"));
+  }
+
+  @Test
+  public void testTwoStringsOfSameLengthThatAreNotEqualButArePermutations() {
+    assertTrue(
+        "String 'ab' is a permutation of 'ba' and should return true",
+        chapterOne.isPermutation("ab", "ba"));
+  }
+}
